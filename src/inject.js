a2a2cc60f52709c05986188a197632fb039b6f0e
zoumiaojiang <zoumiaojiang@gmail.com>
2017-08-09 15:41:47 +0800

    bugfix

--- src/inject.js
+++ src/inject.js
@@ -14,20 +14,21 @@ export default function inject(publicDir) {
     fs.readdirSync(publicDir).forEach(item => {
         let itemPath = path.resolve(publicDir, item);
 
-        if (fs.statSync(itemPath).isFile() && /\.html$/.test(item)) {
-            let indexHTMLPath = path.join(publicDir, item);
-            let fileContent = fs.readFileSync(indexHTMLPath, 'utf-8').toString();
+        if (fs.statSync(itemPath).isFile()) {
+            if (/\.html$/.test(item)) {
+                let indexHTMLPath = path.join(publicDir, item);
+                let fileContent = fs.readFileSync(indexHTMLPath, 'utf-8').toString();
 
-            // if it has not been injected before
-            if (!fileContent.includes(`${injectScript}</body></html>`)) {
-                let injectedContent
-                    = fileContent.replace(/<\/body>\s*<\/html>\s*$/, `${injectScript}</body></html>`);
-                fs.writeFileSync(indexHTMLPath, injectedContent);
+                // if it has not been injected before
+                if (!fileContent.includes(`${injectScript}</body></html>`)) {
+                    let injectedContent
+                        = fileContent.replace(/<\/body>\s*<\/html>\s*$/, `${injectScript}</body></html>`);
+                    fs.writeFileSync(indexHTMLPath, injectedContent);
+                }
             }
         }
         else if (fs.statSync(itemPath).isDirectory()) {
-            publicDir = publicDir + path.sep + item;
-            inject(publicDir);
+            inject(itemPath);
         }
     });
 }
