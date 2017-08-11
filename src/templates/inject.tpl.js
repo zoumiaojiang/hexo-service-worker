1a8cb7b662964e3f442a36b72aa7f53b86d79a31
zoumiaojiang <zoumiaojiang@gmail.com>
2017-08-11 14:16:59 +0800

    lighthouse cannot found service worker bugfix

--- src/templates/inject.tpl.js
+++ src/templates/inject.tpl.js
@@ -3,11 +3,11 @@
  * @author mj(zoumiaojiang@gmail.com)
  */
 
-window.onload = function () {
-    var script = document.createElement('script');
-    var firstScript = document.getElementsByTagName('script')[0];
-    script.type = 'text/javascript';
-    script.async = true;
-    script.src = '/sw-register.js?v=' + Date.now();
-    firstScript.parentNode.insertBefore(script, firstScript);
-};
+
+var script = document.createElement('script');
+var firstScript = document.getElementsByTagName('script')[0];
+script.type = 'text/javascript';
+script.async = true;
+script.src = '/sw-register.js?v=' + Date.now();
+firstScript.parentNode.insertBefore(script, firstScript);
+
